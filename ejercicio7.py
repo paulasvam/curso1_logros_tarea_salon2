cfff258fb79ccb8a672f8ba2600995199b63f1d6
paulasvam <anapaulacv2004@gmail.com>
2025-03-11 11:31:27 -0400

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,12 @@
+calificacion= int(input("ingresar la calificacion de la persona:"))
+
+if calificacion >=90:
+    print("su calificacion se encuentra en el rango A")
+elif calificacion >=80:
+    print("su calificacion se encuentra en el rango B")
+elif calificacion >=70:
+    print("su calificacion se encuentra en el rango C")
+elif calificacion >=60:
+    print("su calificacion se encuentra en el rango D")
+else:
+    print("su calificacion se encuentra en el rango E")
---
+++ ejercicio8.py
@@ -0,0 +1,8 @@
+numero= int(input("ingresar numero:"))
+
+if numero >0:
+    print("el numero es positivo")
+elif numero <0:
+    print("el numero es negativo ")
+else:
+    print("el numero es cero ")
