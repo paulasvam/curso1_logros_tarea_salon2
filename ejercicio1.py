192affa54c83c6b47a15db0098483f882405b750
paulasvam <anapaulacv2004@gmail.com>
2025-03-11 08:52:03 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+nombre= input("ingresar su nombre:")
+edad= int(input("ingresar su edad:"))
+pais= input("ingresar pais de nacimiento:")
+
+print(f"muy buenos dias mi nombre es {nombre}, tengo {edad} años y naci en {pais}")
---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+numero1= int(input("ingresar primer numero:"))
+numero2= int(input("ingresar segundo numero:"))
+
+suma= numero1+numero2 
+
+print(f"la suma de los dos numeros anteriormente ingresados es: {suma}")
